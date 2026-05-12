2599588ca342fb6c33ee48c5c9d2e8c600e82d82
Héctor Alejandro Toro Bernal <66646440+hatorob@users.noreply.github.com>
2026-05-12 17:00:18 -0500

    refactor: se modifica el movimiento para dejarlo en una sola linea

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -10,7 +10,10 @@ func _physics_process(delta):
 	var x_input = Input.get_axis("move_left","move_right")
 	# print(y_input)
 	# print(x_input)
-	velocity.y = y_input * speed_player
-	velocity.x = x_input * speed_player
+	# La manera traficional
+	#velocity.y = y_input * speed_player
+	#velocity.x = x_input * speed_player
+	# la manera de unirla en una sola linea
+	velocity = Vector2(x_input, y_input) * speed_player
 	# mueve al cuerpo basado en su propiedad velocity
 	move_and_slide()
