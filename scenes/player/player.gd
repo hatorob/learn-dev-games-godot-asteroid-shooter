ccbfa25e63d6bc0691c1eec9e289d5c0e202b8df
Héctor Alejandro Toro Bernal <66646440+hatorob@users.noreply.github.com>
2026-05-13 11:35:22 -0500

    feat: Agrego el laser y que dispare desde la nave

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 @export var speed_player: float
+@export var laser_scene: PackedScene
 
 # Método _physics_process que esta sincronizado con las físicas del motor, se ejecuta que se procesa colisiones físicas, movimientos, etc.
 # Físicas
 # Movimientos de cuerpos físicos (CharacterBody2D, RigidBody2D)
 # Por defecto se ejecuta 60 veces por segundo y por intervalos fijos
 func _physics_process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		create_laser()
 	var y_input = Input.get_axis("move_up","move_down")
 	var x_input = Input.get_axis("move_left","move_right")
 	# print(y_input)
@@ -17,3 +20,8 @@ func _physics_process(delta):
 	velocity = Vector2(x_input, y_input) * speed_player
 	# mueve al cuerpo basado en su propiedad velocity
 	move_and_slide()
+
+func create_laser():
+	var laser_instance = laser_scene.instantiate()
+	laser_instance.global_position = global_position
+	add_sibling(laser_instance)
